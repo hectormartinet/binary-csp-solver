1490a4582b2209600af58a9505739b017755ae84
Lucas Langlade <lucaslanglade24@gmail.com>
2024-10-06 12:07:36 -0400

    change continue

--- solver.cpp
+++ solver.cpp
@@ -10,14 +10,14 @@ bool Solver::forwardChecking(int x, int a) {
     std::vector<std::pair<int,int>> modifs;
     for (const auto& [y, ijConstraint] : constraints.at(x)) {
         if (!setVariables.count(y)) {
-            unsigned int infeas = 0;
+            unsigned int nbInfeasInDomain = 0;
             for (int b : problem.getDomain(y)) {
                 if (!ijConstraint.feasible(a,b)) {
-                    infeas++;
+                    nbInfeasInDomain++;
                     modifs.push_back(std::make_pair(y, b));
                 }
             }
-            if (infeas == problem.sizeDomain(y)) return false;
+            if (nbInfeasInDomain == problem.sizeDomain(y)) return false;
         }
     }
     for (std::pair<int,int> couple : modifs) {
@@ -39,24 +39,17 @@ bool Solver::solve() {
     if (unsetVariables.empty()) return true;
     int x = *unsetVariables.begin();
     unsetVariables.erase(x);
-    // std::pair<std::unordered_map<int, std::unordered_map<int,std::unordered_map<int,int>>>, std::vector<std::pair<int,int>>> modifs(problem.AC4());
+
     for (int a : problem.getDomain(x)) {
         fixVarValue(x, a);
         if (!forwardChecking(x, a)) {
             unfixVarValue(x);
-            return false;
+            continue;
         }
         if (solve()) return true;
         stepBack();
         unfixVarValue(x);
     }
-    // for (const auto& [x,yCount] : modifs.first) {
-    //     for (const auto& [y,aCount] : yCount) {
-    //         for (const auto& [a,count] : aCount) {
-    //             problem.initCount(x, y, a, count);
-    //         }
-    //     }
-    // }
 
     unsetVariables.emplace(x);
     return false;

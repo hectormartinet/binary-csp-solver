b380f6cc2973296c6858cef9c441e8321ca0eaab
hectormartinet <hectormartinet@yahoo.fr>
2024-10-11 16:55:37 +0200

    add intensive constraints

--- constraint.h
+++ constraint.h
@@ -5,6 +5,9 @@
 #include <unordered_set>
 #include <vector>
 #include <memory>
+#include <functional>
+#include <stdexcept>
+#include <iostream>
 
 class Constraint {
 
@@ -25,7 +28,7 @@ public:
     virtual bool feasible(const std::unordered_map<int,int>& partSol) const=0;
 
     // Get pairs where x=a is not in the domain Dx in input
-    virtual std::vector<std::pair<int,int>> getUselessPairs(const std::unordered_set<int>& Dx) const=0;
+    virtual std::vector<std::pair<int,int>> getUselessPairs(const std::unordered_set<int>&) const{return std::vector<std::pair<int,int>>();};
 
     virtual const std::unordered_set<int>& getSupport(int a) const=0;
     virtual size_t getSupportSize(int value) const=0;
@@ -50,13 +53,37 @@ public:
     bool feasible(int a, int b) const {return list.count(a)>0 && list.at(a).count(b);}
     bool feasible(const std::unordered_map<int,int>& partSol) const{return feasible(partSol.at(x),partSol.at(y));}
 
-    // Get pairs where x=a is not in the domain Dx in input
     std::vector<std::pair<int,int>> getUselessPairs(const std::unordered_set<int>& Dx) const;
 
     const std::unordered_set<int>& getSupport(int a) const {return list.at(a);}
     size_t getSupportSize(int value) const {return list.count(value) ? list.at(value).size():0;}
 
     void display() const;
+};
+
+
+class IntensiveConstraint: public Constraint {
+
+protected:
+    std::function<bool(int,int)> feasibleFunction;
+
+public:
+    IntensiveConstraint(int _x, int _y, std::function<bool(int,int)> _feasibleFunction) : Constraint(_x,_y), feasibleFunction{_feasibleFunction} {}
+
+    std::unique_ptr<Constraint> clone() {return std::unique_ptr<IntensiveConstraint>(new IntensiveConstraint{*this});}
+
+    void addPair(int, int) {throw std::logic_error("Cannot add pair to intensive constraint");};
+    void removePair(int, int){throw std::logic_error("Cannot remove pair from intensive constraint");};
+
+    bool feasible(int a, int b) const{return feasibleFunction(a,b);}
+    bool feasible(const std::unordered_map<int,int>& partSol) const{return feasible(partSol.at(x),partSol.at(y));}
+
+    std::vector<std::pair<int,int>> getUselessPairs(const std::unordered_set<int>&) const{return std::vector<std::pair<int,int>>();};
+
+    const std::unordered_set<int>& getSupport(int) const{throw std::logic_error("Not implemented lol");};
+    size_t getSupportSize(int) const{throw std::logic_error("Not implemented lol");};
+
+    void display() const {std::cout << x << "," << y << ": Intensive constraint"<<std::endl;}
 };
 
 #endif
